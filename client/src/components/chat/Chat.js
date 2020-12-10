9931c6db0360d7b629c22b80bebccb78564c78e5
cyrus8050 <ckmobile8050@gmail.com>
2020-12-10 17:53:39 +0800

    install mongoose

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -1,15 +1,44 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../UserContext';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import io from 'socket.io-client';
+let socket;
 const Chat = () => {
-    const { user, setUser } = useContext(UserContext)
+    const ENDPT = 'localhost:5000';
 
+    const { user, setUser } = useContext(UserContext);
+    let { room_id, room_name } = useParams();
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    useEffect(() => {
+        socket = io(ENDPT);
+        socket.emit('join', { name: user.name, room_id, user_id: user.id })
+    }, [])
+    useEffect(() => {
+        socket.on('message', message => {
+            setMessages([...messages, message])
+        })
+    }, [messages])
+    const sendMessage = event => {
+        event.preventDefault();
+        if (message) {
+            console.log(message)
+            socket.emit('sendMessage', message, room_id, () => setMessage(''))
+        }
+    }
     return (
         <div>
+            <div>{room_id} {room_name}</div>
             <h1>Chat {JSON.stringify(user)}</h1>
-            <Link to={'/'}>
-                <button>go to home</button>
-            </Link>
+            <pre>{JSON.stringify(messages, null, '\t')}</pre>
+            <form action="" onSubmit={sendMessage}>
+                <input type="text"
+                    value={message}
+                    onChange={event => setMessage(event.target.value)}
+                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+                />
+                <button>Send Message</button>
+            </form>
         </div>
     )
 }
