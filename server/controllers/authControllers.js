76fa6ed6806af4b25fe357b838ab1e90ad09e9e3
cyrus8050 <ckmobile8050@gmail.com>
2020-12-19 16:46:05 +0800

    validating the errors

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,13 +1,31 @@
 const User = require('../models/User');
+
+const alertError = err => {
+    let errors = { name: '', email: '', password: '' };
+    // console.log(err.message)
+    if (err.message.includes('user validation failed')) {
+        console.log('err.errors', err.errors)
+        console.log('err.errors values 0', Object.values(err.errors)[0])
+        console.log('err.errors values 1', Object.values(err.errors)[1])
+        Object.values(err.errors).forEach(error => {
+            console.log(error.properties)
+        })
+        Object.values(err.errors).forEach(({ properties }) => {
+            errors[properties.path] = properties.message
+        })
+    }
+    console.log(errors)
+    return errors
+}
 module.exports.signup = async (req, res) => {
     const { name, email, password } = req.body;
     try {
         const user = await User.create({ name, email, password });
         res.status(201).json({ user });
     } catch (error) {
-        console.log(`error message: ${error.message}`)
-        console.log(`error code: ${error.code}`)
+        const errors = alertError(error)
         // res.status(400).send('Fail to create user')
+        res.status(400).json({ errors })
     }
 
 }
